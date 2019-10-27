419910fa88f2c569e9065bd52f898498da95087e
grant thomas <grantwilliamthomas@hotmail.co.uk>
2019-10-27 22:33:21 +0100

    finished creating basic api

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,7 @@
 class Restaurant < ApplicationRecord
   belongs_to :user
   has_many :comments
+
+  validates :name, presence: true, uniqueness: true
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :restaurants, only: [ :index, :show ]
+      resources :restaurants, only: [ :index, :show, :update, :create, :destroy ]
     end
   end
+
+  devise_for :users
 # root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
