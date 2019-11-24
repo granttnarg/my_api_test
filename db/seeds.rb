1562a804945bad6b77c7ac5ea093924be3e2ab99
grant thomas <grantwilliamthomas@hotmail.co.uk>
2019-11-24 18:51:44 +0100

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first
+Comment.destroy_all
+Restaurant.destroy_all
+User.destroy_all
+
+
 user = User.create(email:"test@testing.com", password: "testtest")
+puts "user created"
 
-otto = Restaurant.create(user: user, name: "Ottolenghi", address: "123 Otto Street")
+otto = Restaurant.create(user: user, name: "Ottolenghi", address: "287 Upper Street, London")
+rovi = Restaurant.create(user: user, name: "Rovi", address: "59 Wells st, London")
+ducksoup = Restaurant.create(user: user, name: "Duck Soup", address: "41 Dean st, London")
 
-Comment.create(user: user, restaurant: otto, content: "Lovely stuff")
+puts "3 London Restaurants created"
+
+gazzo = Restaurant.create(user: user, name: "Gazzo", address: "Hobrechtstraße 57, Berlin")
+time_raue = Restaurant.create(user: user, name: "Time Raue", address: "Rudi-Dutschke-Straße 26, Berlin")
+
+puts "3 London Restaurants created"
+
+Comment.create(user: user, restaurant: otto, content: "Top knotch")
+Comment.create(user: user, restaurant: gazzo, content: "Pizza Heaven")
+
+puts "2 Comments created"
